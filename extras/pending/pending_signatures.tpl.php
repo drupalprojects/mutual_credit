b918d2a395d663eada89976142c235c6d8d24a91
Matthew Slater <matslats@gmail.com>
2012-06-02 18:04:13 +0100

    overhaul of the pending module. Now works with actions and triggers

---
+++ extras/pending/pending_signatures.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * theme implementation of pending_signatures
+ * show the $transaction->pending signatures, signed and unsigned, with links
+ *
+ * variables are
+ * $pending signatures - original data pulled from transaction object
+ * $signatories - intermediate data saying how to theme it, with links and access control done
+ * $signoff_link - is a link as a render array, checked for access control
+ * $rows produced by template_preprocess_pending_signatures, with themed cells and links as render arrays
+ * $finished = boolean whether or not the transaction->state is TRANSACTION_STATE_FINISHED
+ */
+$title = $finished ? t('Signed by') : t('Awaiting Signatures');
+?>
+<div style ="float:right" id ="pending-signatures">
+  <h2><?php print $title; ?></h2>
+  (<?php print render($signoff_link); ?>)
+  <style>#pending-signatures .signed{background-image: url('/misc/message-24-ok.png');background-repeat: no-repeat;}
+  #pending-signatures .pending{background-image: url('/misc/message-24-warning.png');background-repeat: no-repeat;}
+  #pending-signatures .sign-link{}</style>
+  <?php print render($table); ?>
+</div>
