ae1af368e4c8d31be3d1dcaf4ef875c3d2f91640
Matthew Slater <matslats@gmail.com>
2012-12-15 19:01:04 +0200

    improved signature theming

--- transaction.tpl.php
+++ transaction.tpl.php
@@ -35,7 +35,7 @@ $replacements = array(
 );
 if ($view_mode == 'certificate') {
   print render($pending_signatures); //floating the right, by default
-  $replacements['!worth'] = '<span class = "quantity">'. $replacements['!worth'] .'</div>';
+  $replacements['!worth'] = '<span class = "quantity">'. $replacements['!worth'] .'</span>';
   $certificate_string = t(
     'On @recorded<br />!payer <strong>paid</strong> !payee</br />the sum of !worth',
     $replacements
@@ -43,6 +43,7 @@ if ($view_mode == 'certificate') {
   $certificate_string = str_replace('<br/>', '<br /><br />', $certificate_string);
 }
 ?>
+<!--transaction.tpl.php-->
 <div class = "<?php print $classes; ?>">
   <?php if ($view_mode == 'certificate') : ?>
     <?php print $certificate_string; ?>
