fa3d158fda0237c3547206f7cd149ce95444abcf
Matthew Slater <matslats@gmail.com>
2013-03-24 14:14:10 -0500

    balance history gchart now working well, with sampling; alterations to default form paths under transact/; improvements to entity api integration and requirements warning about using entity token module

--- extras/balance_history/balance_history.tpl.php
+++ extras/balance_history/balance_history.tpl.php
@@ -1,5 +1,6 @@
 <script type="text/javascript" src="http://www.google.com/jsapi"></script>
-<script type="text/javascript">google.load('visualization', '1', {packages: ['corechart']});</script>
+<script type="text/javascript">
+  google.load('visualization', '1', {packages: ['corechart']});</script>
 <?php
 ////$Id: balance_history.tpl.php,v 1.3 2010/12/08 11:43:18 matslats Exp $
 
@@ -23,20 +24,23 @@
 
 $color_sequence = array('21a0db', '2aab49');
 foreach ($histories as $currcode => $history) {
-  $colors[] = "'".next($color_sequence) ."'";
+  $colors[] = "'".array_pop($color_sequence) ."'";
   foreach ($history as $timestamp => $balance) {
     $timeline[$timestamp][$currcode] = $balance;
   }
 }
+if (empty($timeline))return '';
+//$timeline is now a list of times and changes of balance in currencies
 ksort($timeline);
+//what we need is a list of times with both balances per moment
 $prev = array();
 foreach ($timeline as $timestamp => $balances) {
   $timeline[$timestamp] = $balances + $prev;
   $prev = $timeline[$timestamp];
 }
-$cache_id = 'uid'.$account->uid.implode('',array_keys($histories));?>
+$id = 'uid-'.$account->uid.'-'.implode('',array_keys($histories));?>
 <script type="text/javascript">
-function drawbalancehistory() {
+function drawBalanceHistory() {
   var data = new google.visualization.DataTable();
   data.addColumn('date', 'Date');
 <?php foreach (array_keys(current($timeline)) as $currcode) { ?>
@@ -47,13 +51,14 @@ function drawbalancehistory() {
   $date = 'new Date("'.date('m/d/Y', $timestamp).'")';?>
   data.addRow([<?php print $date; ?>, <?php print implode(', ', $balances);?>]);
 <?php } ?>
-  new google.visualization.LineChart(document.getElementById('<?php print $cache_id;?>')).draw(data, {
-      curveType: "<?php print $curvetype; ?>",
-      width: 250,
-      height: 200,
-      colors: [<?php print implode(', ', $colors);?>],
-  });
+  var options = {
+    curveType: "<?php print $curvetype; ?>",
+    width: 250,
+    height: 200,
+    colors: [<?php print implode(', ', $colors);?>],
+  }
+  new google.visualization.LineChart(document.getElementById('<?php print $id; ?>')).draw(data, options);
 }
-google.setOnLoadCallback(drawbalancehistory);
+google.setOnLoadCallback(drawBalanceHistory);
 </script>
-<div id="<?php print $cache_id;?>" style="width: 250px; height: 200px;"></div>
+<div id="<?php print $id;?>" style="width:<?php print $width; ?>px; height:<?php print intval(3*$width/4);?> px;"></div>
