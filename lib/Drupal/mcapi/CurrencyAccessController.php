f370a9d11e368a9d6c13692f0555fc91e1bcb10b
Gordon Heydon <gordon@heydon.com.au>
2013-11-02 11:54:51 +1100

    Change Currency Access controller to be able to use the currency access functions.

--- lib/Drupal/mcapi/CurrencyAccessController.php
+++ lib/Drupal/mcapi/CurrencyAccessController.php
@@ -22,8 +22,15 @@ class CurrencyAccessController extends EntityAccessController {
    * {@inheritdoc}
    */
   public function checkAccess(EntityInterface $entity, $operation, $langcode, AccountInterface $account) {
-    //TODO: If the currency has non-deleted transaction then stop the currency from being deleted.
-    return $account->hasPermission('configure all currencies') || (isset($entity->uid) && $entity->uid == $account->id());
+    if (isset($entity->access[$operation])) {
+      $callback = strtok($entity->access[$operation], ':');
+      $arg = strtok(':');
+      return in_user_chooser_segment($callback, array($arg), $account->id());
+    }
+    else {
+      //TODO: If the currency has non-deleted transaction then stop the currency from being deleted.
+      return $account->hasPermission('configure all currencies') || (isset($entity->uid) && $entity->uid == $account->id());
+    }
   }
 
 }
