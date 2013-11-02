e2c643a670d5928a77f293674501d1e6563fd3e4
Gordon Heydon <gordon@heydon.com.au>
2013-11-02 11:55:17 +1100

    Fix up the loading of the Currency to expand the divisions.

--- lib/Drupal/mcapi/CurrencyStorageController.php
+++ lib/Drupal/mcapi/CurrencyStorageController.php
@@ -10,4 +10,19 @@ namespace Drupal\mcapi;
 use Drupal\Core\Config\Entity\ConfigStorageController;
 
 class CurrencyStorageController extends ConfigStorageController {
+  /**
+   * {@inheritdoc}
+   */
+  protected function attachLoad(&$queried_entities, $revision_id = FALSE) {
+    parent::attachLoad($queried_entities, $revision_id);
+
+    foreach ($queried_entities as $entity) {
+      if ($entity->display['divisions'] == CURRENCY_DIVISION_MODE_CUSTOM) {
+        foreach(explode("\n", $entity->display['divisions_setting']) as $line) {
+          list($cent, $display) = explode('|', $line);
+          $entity->display['divisions_allowed'][$cent] = trim($display);
+        }
+      }
+    }
+  }
 }
