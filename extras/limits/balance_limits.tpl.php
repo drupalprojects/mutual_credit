08afb855466be2290cb569fb90f4826e6cd2c87e
Matthew <matslats@gmail.com>
2015-03-18 16:40:30 +0000

    make the date popup work; improve limits gage template; allow acknowledgement currencies to have balance limits; improve upgrade from d6 for pending transactions; improve upgrade from d6 currency bug

--- extras/limits/balance_limits.tpl.php
+++ extras/limits/balance_limits.tpl.php
@@ -44,3 +44,5 @@ function drawGauge() {
 google.setOnLoadCallback(drawGauge);
 </script>
 <div id ="<?php print $id; ?>" class = "limits-gauge" style ="width:<?php print $balance_gauge_pixels; ?>px; height:<?php print $balance_gauge_pixels; ?>px"></div>
+<div class = "limits">Min: <?php echo $min; ?> | Max: <?php echo $max; ?> hours</div>
+
